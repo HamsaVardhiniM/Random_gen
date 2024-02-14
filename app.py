5c8cd2702484b7a9fb84178f0fec4a39a42627ae
Hamsa Vardhini M <121292308+HamsaVardhiniM@users.noreply.github.com>
2024-02-14 21:40:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+from flask import Flask, jsonify
+import random
 
+app = Flask(__name__)
+
+@app.route('/random_number', methods=['GET'])
+def generate_random_number():
+    random_number = random.randint(1, 1000)
+    return jsonify({'random_number': random_number})
+
+if __name__ == '__main__':
+    app.run(debug=True)
